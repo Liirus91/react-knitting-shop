be8ff9e13192e74ffb4823739b6828d53358736d
Liirus91 <liirus91@gmail.com>
2023-02-06 18:43:06 +0500

    feat: install redux toolkit

---
+++ src/redux/slices/filterSlice.ts
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  value: 0,
+};
+
+export const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+  },
+});
+
+export const { increment } = filterSlice.actions;
+
+export default filterSlice.reducer;
